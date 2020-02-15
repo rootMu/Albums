82b5253f7f088a29ae6b49a440feed2daf0e409a
Matthew Howells <matthewhowells@live.com>
2020-02-15 19:54:15 +0000

    setup dagger for viewmodel

--- app/src/main/java/com/matthew/albums/AlbumViewModel.kt
+++ app/src/main/java/com/matthew/albums/AlbumViewModel.kt
@@ -1,5 +1,17 @@
 package com.matthew.albums
 
 import androidx.lifecycle.ViewModel
+import com.matthew.albums.di.DaggerViewModelInjector
+import com.matthew.albums.di.ViewModelInjector
 
-class AlbumViewModel: ViewModel()
+class AlbumViewModel: ViewModel() {
+
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+        .builder()
+        .build()
+
+    init{
+        injector.inject(this)
+    }
+
+}
